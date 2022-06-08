273f19f0275538108a7970298d882776553612e4
Antenor Zapata <antenorzapata@gmail.com>
2022-06-07 22:21:30 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignupController } from './signup'
+import { MissingParamError } from '../errors/missing-params-errors'
 
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -13,7 +14,7 @@ describe('Signup Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     // expect(httpResponse.message).toBe('User name must be provided')
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Should return 400 if no email is provided', () => {
@@ -27,7 +28,7 @@ describe('Signup Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    // expect(httpResponse.message).toBe('User name must be provided')
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,20 @@
+import { MissingParamError } from '../errors/missing-params-errors'
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SignupController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new Error('Missing param: name') }
+      return {
+        statusCode: 400,
+        body: new MissingParamError('name')
+      }
     }
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new Error('Missing param: email') }
+      return {
+        statusCode: 400,
+        body: new MissingParamError('email')
+      }
     }
+    return { statusCode: 200, body: {} }
   }
 }
